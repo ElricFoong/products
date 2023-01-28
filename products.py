a91b86824b2b7df9be83e5e46bef393e3fb346e9
ElricFoong <elricfoong@gmail.com>
2023-01-28 19:17:50 +1100

    add header and encoding utf-8

--- products.py
+++ products.py
@@ -5,15 +5,16 @@ while True:
     name = input("Products name ")
     if name == "q":
         break
-    price = input("Products price ")
+    price = int(input("Products price "))
     products.append([name, price])
 
 print(products)
 
 
 for product in products:
-    print(product[0], "price is ", product[1])
+    print(product[0], "价格是 ", product[1])
 
-with open("products.csv", "w") as f:
+with open("products.csv", "w", encoding="utf-8") as f:
+    f.write("商品，价格 \n")
     for p in products:
-        f.write(p[0] + "," + p[1] + "\n")
+        f.write(p[0] + "," + str(p[1]) + "\n")
