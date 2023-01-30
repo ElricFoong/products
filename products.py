087a0723e59d3859ff36bc6fa2aafef148980461
ElricFoong <elricfoong@gmail.com>
2023-01-30 18:25:44 +1100

    add read file

--- products.py
+++ products.py
@@ -1,20 +1,22 @@
 products = []
-print("Please type q for exit ")
-
+#  读取档案
+with open("products.csv", "r", encoding="utf-8")as f:
+    for lines in f:
+        if "商品, 价格" in lines:
+            continue
+        name, price = lines.strip().split(",")
+        products.append([name, price])
+print(products)
+#  输入商品名称与价格
 while True:
-    name = input("Products name ")
+    name = input("请输入商品名称 ")
     if name == "q":
         break
-    price = int(input("Products price "))
+    price = input("请输入商品价格 ")
     products.append([name, price])
-
 print(products)
-
-
-for product in products:
-    print(product[0], "价格是 ", product[1])
-
-with open("products.csv", "w", encoding="utf-8") as f:
-    f.write("商品，价格 \n")
+# 写入档案
+with open("products.csv", "w", encoding="utf-8")as f:
+    f.write("商品, 价格 \n")
     for p in products:
-        f.write(p[0] + "," + str(p[1]) + "\n")
+        f.write(p[0] + "," + p[1] + "\n")
