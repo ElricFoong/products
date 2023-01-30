fd5f3cc67d0283fe827cef8ab4ad052a13aa8065
ElricFoong <elricfoong@gmail.com>
2023-01-30 18:53:23 +1100

    add import os

--- products.py
+++ products.py
@@ -1,11 +1,14 @@
+import os
 products = []
-#  读取档案
-with open("products.csv", "r", encoding="utf-8")as f:
-    for lines in f:
-        if "商品, 价格" in lines:
-            continue
-        name, price = lines.strip().split(",")
-        products.append([name, price])
+#         检查档案在不在
+if os.path.isfile("products.csv"):
+    with open("products.csv", "r", encoding="utf-8")as f:  # 读取档案
+        for lines in f:
+            if "商品, 价格" in lines:
+                continue
+            name, price = lines.strip().split(",")
+            products.append([name, price])
+
 print(products)
 #  输入商品名称与价格
 while True:
@@ -15,6 +18,10 @@ while True:
     price = input("请输入商品价格 ")
     products.append([name, price])
 print(products)
+
+for p in products:
+    print(p[0], "的价格是", p[1])
+
 # 写入档案
 with open("products.csv", "w", encoding="utf-8")as f:
     f.write("商品, 价格 \n")
