8a4dca82b1103fabcbd7f82db35c74ae8c565b26
ElricFoong <elricfoong@gmail.com>
2023-01-31 20:51:41 +1100

    Refactor

--- products.py
+++ products.py
@@ -1,29 +1,53 @@
 import os
-products = []
-#         检查档案在不在
-if os.path.isfile("products.csv"):
-    with open("products.csv", "r", encoding="utf-8")as f:  # 读取档案
-        for lines in f:
-            if "商品, 价格" in lines:
+
+
+def read_file(filename):
+    products = []
+    with open(filename, "r", encoding="utf-8")as f:
+        for line in f:
+            if "商品,价格" in line:
                 continue
-            name, price = lines.strip().split(",")
+            name, price = line.strip().split(",")
             products.append([name, price])
 
-print(products)
-#  输入商品名称与价格
-while True:
-    name = input("请输入商品名称 ")
-    if name == "q":
-        break
-    price = input("请输入商品价格 ")
-    products.append([name, price])
-print(products)
+    return products
 
-for p in products:
-    print(p[0], "的价格是", p[1])
 
-# 写入档案
-with open("products.csv", "w", encoding="utf-8")as f:
-    f.write("商品, 价格 \n")
+def user_input(products):
+    while True:
+        name = input("请输入商品名称 ")
+        if name == "q":
+            break
+        price = int(input("请输入商品价格 "))
+        products.append([name, price])
+    print(products)
+    return products
+
+
+def print_products(products):
     for p in products:
-        f.write(p[0] + "," + p[1] + "\n")
+        print(p[0], "的价格是, ", p[1])
+
+
+def write_file(filename, products):
+    with open(filename, "w", encoding="utf-8")as f:
+        f.write("商品,价格\n")
+        for p in products:
+            f.write(p[0] + "," + str(p[1]) + "\n")
+
+
+def main():
+    filename = "products.csv"
+    products = []
+    if os.path.isfile(filename):
+        print("找到档案")
+        products = read_file(filename)
+    else:
+        print("找不到档案")
+
+    products = user_input(products)
+    print_products(products)
+    write_file("products.csv", products)
+
+
+main()
